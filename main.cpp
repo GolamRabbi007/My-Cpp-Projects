1d6e45ca3dbdf80bb05af189cf3b2cb6b12166f2
DM Golam Rabbi <golamrabbi1993@gmail.com>
2023-02-04 22:47:12 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+//    void multiply_by_30 (){
+//        double input {};
+//        cout<<"input your data "<<endl;
+//        cin>>input;
+//        cout<<" Your result is "<< input << " * "<< 30 << " = "<<input * 30;
+//        
+//    }
+//	
+//    int main(){
+//        multiply_by_30();
+//        return 0;
+//    }
+//
+//const double pi {3.14159};
+//
+//double calc_area_circle(double radius){
+//    return radius * radius * pi;
+//}
+//
+//double calc_cylinder_volume (double height, double radius){
+//    return calc_area_circle(radius) * height;
+//}
+//
+//
+//
+//void circle_area (){
+//    cout<<"Please input the radius: "<<endl;
+//    double radius{};
+//    cin>>radius;
+//    cout<<"Area of the circle is "<<calc_area_circle(radius)<<endl;
+//}
+//
+//void cylinder_volume (){
+//    cout<<"Enter the height of Cylinder "<<endl;
+//    double height {};
+//    cin>>height;
+//    cout<<"Enter the radius of the cylinder "<<endl; 
+//    double radius {};
+//    cin>>radius;
+//    cout<<"The volume of the cylinder is "<<calc_cylinder_volume(height, radius)<<endl;
+//}
+//
+//
+//int main(){
+//    circle_area();
+//    cylinder_volume();
+//    
+//    return 0;
+//    
+
+
+//void param_test (int){
+//    int formal {100};
+//    cout<<formal<<endl;
+//}
+//
+//int main (){
+//    int actual {40};
+//    cout<<actual<<endl;
+//    param_test(actual);
+//    cout<<actual<<endl;
+//    return 0;
+//}
+
+
+//
+//void say_hello (string name) {
+//    cout<<"Hello " <<name<<endl;
+//}
+//
+//int main (){
+//    say_hello("Frank");
+//    return 0;
+//}
+
+//void pass_by_value (string s = "changed" ){
+//    cout<<s<<endl;
+//}
+//
+//int main (){
+//    pass_by_value ();
+//    
+//}
+
