4f806d579541fdfde629e217912fa0be11b1f97b
DM Golam Rabbi <golamrabbi1993@gmail.com>
2023-02-04 22:18:19 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Section 5
+// Basic I/O using cin & cout 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" <<endl;
+//    cout <<"World"<<endl;
+//    cout <<"Hello"<<"World"<<endl;
+//    cout <<"Hello"<<"World!\n"<<endl;
+//    cout <<"Hello\nOut\nThere\n"<<endl;
+    return 0;
+} 
+
