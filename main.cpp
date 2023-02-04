8188a9eb7d329df8221d92048031eb464bf70a25
DM Golam Rabbi <golamrabbi1993@gmail.com>
2023-02-04 22:40:06 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//int main ()
+//{
+//   for (int i {1}; i <= 10; ++i )
+//       {
+//           if(i % 2 == 0){cout<<i<<endl;}
+//       }
+//       return 0;
+//}
+
+
+//int main ()
+//{
+//    int scores [] {100,99,98};
+//    for (int i {0} ; i<3 ; ++i )
+//    {
+//        cout<< scores[i]<<endl;
+//    }
+//        
+//}
+
+
+//int main ()
+//{
+//    for (int i {1} ;i<=10; i+=1)
+//        {if(i%2==0){cout<<i<<endl;}
+//        else {cout<<"Odd integer"<<endl;}}
+//    return 0;
+//}
+
+//int main ()
+//{
+//    for (int i{2},j{3} ; i<=5 || j<=3 ; i+=1,j+=1)
+//         {cout<< i << " * " <<j << " = "<< i*j<<endl;}
+//return 0;
+//}
+
+//int main ()
+//{
+//    vector <int> nums {100,90,80};
+//    for (unsigned i {0}; i<nums.size(); i+=1)
+//         {cout<<nums[i]<<endl;}
+//    return 0;
+//}
+
+
+//int main ()
+//{
+//    vector<double> temps {87.2, 77.1, 80.0, 72.5};
+//    
+//    double average_temps {};
+//    double sums {};
+//    
+//    
+//    for (double temp : temps)
+//        
+//        
+//    
+//    sums += temp;
+//    average_temps = sums / temps.size(); 
+//    
+//    {cout<<average_temps<<endl;}
+//    return 0;
+//}
+
+//int main ()
+//{
+//    cout<<"Enter a starting countdown value: "<<endl;
+//    int limit;
+//    cin>>limit;
+//    int i{};
+//    for (i = limit; i; --i)
+//    {cout<<"i = "<<i<<"\n";}
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    cout<<"Type a count down value: "<<endl;
+//    int value {};
+//    cin>>value;
+//    for (int j {value}; j; --j)
+//    {cout<<"j = "<<j<<endl;}
+//    return 0;
+//}
+
+
+//int main ()
+//{
+//    vector <int> nums {10,20,30};
+//    for (int i {}; i<nums.size(); ++i)
+//    {cout<<nums[i]<<endl;}
+//    return 0;
+//}
+
+
+
+//int main ()
+//{
+//    vector <int> scores {10,20};
+//    for (auto score : scores)
+//    {cout<< score<<endl;}
+//    return 0;
+//}
+
+
+//int main ()
+//{
+//    for (int i{}; i<100 ; ++i)
+//    {if (i % 2 == 0){cout <<i<<endl;}}
+//    
+//    return 0;
+//}
+
+
+//int main ()
+//{
+// vector <double> temps {87.9,77.9,80.0,72.5};
+// double average_temp {};
+// double sum_of_temps {};
+// 
+// for (auto temp: temps)
+// 
+// sum_of_temps += temp;
+// average_temp = sum_of_temps / temps. size();
+// 
+// if (temps.size() != 0)
+//{
+//    cout<<average_temp<<endl;
+//}
+//
+//cout<<average_temp<<endl;   
+// 
+// 
+// return 0;  
+//}
+
+
+
+//int main () (Error found)
+//{
+//    int num {};
+//    cout<<"enter a number to be in loop: "<<endl;
+//    cin>>num;
+//    int multiplier {10};
+//    for (int i {num*multiplier} ; (num*multiplier)<=100 ; i+=multiplier)
+//    {cout<<i<<endl;}
+//    return 0;
+//}
