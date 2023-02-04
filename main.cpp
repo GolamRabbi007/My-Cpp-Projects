100e4ec9ed2a58b95ef7694e41d77cc36c2b0f39
DM Golam Rabbi <golamrabbi1993@gmail.com>
2023-02-04 22:38:54 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int input1;
+    cout<<"Enter your favourite number between 1 & 100: ";
+    cin>> input1;
+    cout<<"Amazing!! That's my favourite number too!!"<<std::endl;
+    cout<<"No really!!"<<input1<< " is my favourite number!!"<<std::endl;
+}
