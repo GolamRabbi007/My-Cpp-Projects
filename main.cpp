f9da2df9322b06a6d010b4224bab563a4bd3e233
DM Golam Rabbi <golamrabbi1993@gmail.com>
2023-02-04 22:33:11 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,58 @@
+//this operator is all about equality & inequality equality operator is: "==" and inequality operator is: "!="
+
+
+#include <iostream>
+using namespace std;
+int main()
+//{
+//	bool equal_result {false};
+//    bool not_equal_result {false};
+//	int num1{}, num2{};
+//    
+//    
+//    
+//    cout<<boolalpha;
+//    cout<<"Enter 2 numbers separated by space: \n"<<endl;
+//    cin>> num1>>num2;
+//    equal_result = num1==num2;
+//    cout<<"For equal result: "<<equal_result<<endl;
+//    not_equal_result=num1 != num2;
+//    cout<<"For not equal result: "<<not_equal_result<<endl;
+//    return 0;
+//}
+
+//{
+//	bool equal_result {false};
+//    bool not_equal_result {false};
+//	char char1{}, char2{};
+//    
+//    
+//    
+//    cout<<boolalpha;
+//    cout<<"Enter 2 characters separated by space: \n"<<endl;
+//    cin>> char1>>char2;
+//    equal_result = char1==char2;
+//    cout<<"For equal result: "<<equal_result<<endl;
+//    not_equal_result=char1 != char2;
+//    cout<<"For not equal result: "<<not_equal_result<<endl;
+//    return 0;
+//}
+
+
+{
+	bool equal_result {false};
+    bool not_equal_result {false};
+	double num1{}; 
+    double num2{};
+    
+    
+    
+    cout<<boolalpha;
+    cout<<"Enter 2 double separated by space: \n"<<endl;
+    cin>> num1>>num2;
+    equal_result = num1==num2;
+    cout<<"For equal result: "<<equal_result<<endl;
+    not_equal_result=num1 != num2;
+    cout<<"For not equal result: "<<not_equal_result<<endl;
+    return 0;
+}
